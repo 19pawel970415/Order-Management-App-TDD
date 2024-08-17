7db4bee051d03362745d35cc833dbeffa1d31177
19Pawel97 <19pawel9772@gmail.com>
2024-08-17 16:03:29 +0200

    Overriding equals and hashcode

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
 
@@ -32,5 +33,16 @@ public class Order {
         return totalAmount;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
