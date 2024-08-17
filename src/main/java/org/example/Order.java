06d146a5b66f7f629b7ff4c5cc26957e3790ab5d
19Pawel97 <19pawel9772@gmail.com>
2024-08-17 13:50:08 +0200

    Adding Order class

---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.List;
+
+public class Order {
+
+    private String id;
+    private String customerName;
+    private List<String> items;
+    private double totalAmount;
+
+}
